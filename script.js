5654a7552482ca98e22aec8c0aa9bbdfa9be9f07
jnsltk <janos.litkei@gmail.com>
2022-04-19 20:43:42 +0800

    Add reset button and option for changing grid size

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('#btn');
 let gridNumber = 16;
 
-
+btn.addEventListener('click', resetCanvas)
 
 function drawCanvas() {
     container.setAttribute('style', `grid-template-columns: repeat(${gridNumber}, 1fr); 
@@ -16,5 +17,14 @@ function drawCanvas() {
 }
 function changeColor(e) {
     e.target.style.backgroundColor = 'black';
+}
+function resetCanvas() {
+    gridNumber = parseInt(prompt('Please enter a number between 1 and 100 for the number of squares per side for the new grid', '16'));
+    if (gridNumber > 100 || gridNumber < 0) {
+        alert('Please input a valid number and try again!');
+        return;
+    }
+    container.innerHTML = '';
+    drawCanvas();
 }
 drawCanvas();
