5e3df52ce2faa18c941bcb2e7fcb40f8a349828d
jnsltk <janos.litkei@gmail.com>
2022-04-19 20:17:29 +0800

    Add "drawing" functionality

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const container = document.querySelector('.container');
 let gridNumber = 16;
 
-container.setAttribute('style', `grid-template-columns: repeat(${gridNumber}, 1fr); grid-template-rows: repeat(${gridNumber}, 1fr);`)
 
-for (let i = 0; i < gridNumber * gridNumber; i++) {
-    drawCanvas();
-}
 
 function drawCanvas() {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    container.appendChild(pixel);
+    container.setAttribute('style', `grid-template-columns: repeat(${gridNumber}, 1fr); 
+        grid-template-rows: repeat(${gridNumber}, 1fr);`);
+
+    for (let i = 0; i < gridNumber * gridNumber; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.addEventListener('mouseenter', changeColor)
+        container.appendChild(pixel);
+    }
 }
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
+drawCanvas();
